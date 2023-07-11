166b89f607e824718d015e7445f9420e7cc7212f
andev70 <anupalnath70@gmail.com>
2023-07-12 01:58:21 +0530

    ui modification

---
+++ src/components/navbar.svelte
@@ -0,0 +1,46 @@
+<script>
+  import avatar from "../soflex-assets/images/avatar/01.jpg"
+</script>
+<style>
+  .hamburger{
+    font-size:20px;
+  }
+
+  .all-nav-btns{
+    column-gap: 8px;
+  }
+
+  .logo-parent{
+    font-size:24px;
+  }
+</style>
+
+<nav class="w-full h-[56px] bg-white px-3 flex justify-between items-center">
+<!-- logo here start -->
+   <span class="logo-parent w-10 h-10  rounded-[6.4px]">
+<button class="logo-button w-full h-full rounded-[6.4px] flex justify-center items-center">
+  <i class="bi bi-megaphone-fill text-blue-500 "></i>
+    </button>
+  </span>
+  <!-- logo finished -->
+
+  <!-- other nav icons or buttons -->
+  <div class="all-nav-btns h-full flex items-center w-max">
+<!-- messagebird -->
+    <button class=" flex justify-center items-center w-10 h-10 rounded-[6.4px] bg-[#eef0f2]">
+      <i class="bi bi-messenger text-gray-700"></i>
+    </button>
+
+<!-- notifications -->
+    <button class=" flex justify-center  items-center w-10 h-10 rounded-[6.4px] bg-[#eef0f2]">
+      <i class="bi bi-bell-fill text-gray-700"></i>
+    </button>
+       <!-- profile -->
+    <button class=" flex justify-center items-center w-10 h-10 rounded-[6.4px] bg-[#eef0f2]">
+      <i class="bi bi-list hamburger text-gray-700" ></i>
+    </button>
+
+  </div>
+<!-- other nav icons finished -->
+</nav>
+
---
+++ src/components/post-area.svelte
@@ -0,0 +1,27 @@
+<script>
+  import avatar from "../soflex-assets/images/avatar/02.jpg"
+</script>
+<style>
+  .my-profile{
+    font-size:15px;
+  }
+</style>
+
+
+
+
+<section class="pt-[16px] bg-[#eef0f2] post-greate-parent w-full h-max flex justify-center">
+<div class="profile-parent-all  px-4  w-[369.09px]    flex gap-x-3 items-center">
+    <div class="profile-details w-[48px] h-[48px] rounded-full ">
+<button class="h-full w-full rounded-full outline-none">
+      <img src={avatar} class="w-full h-full rounded-full" alt="PF"/>
+</button>
+    </div>
+<!-- input -->
+    <button class="got-pf h-max">
+      <h1 class="my-profile font-semibold font-sans text-[#14191e]">
+       My profile
+      </h1>
+    </button>
+  </div>
+</section>
